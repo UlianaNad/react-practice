1159937f93a3a3947b9f6d6d2ff96acdf21566ff
Uliana Nadorozhna <unadorozhna1@gmail.com>
2024-06-26 15:49:02 +0300

    added createColor component

---
+++ src/components/RandomColor/index.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+const RandomColor = () => {
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+  const [color, setColor] = useState("#000000");
+
+  const randomColorUtility = (length) => {
+    return Math.floor(Math.random() * length);
+  };
+
+  const handleCreateRandomHexColor = () => {
+    const hex = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F"
+    ];
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[randomColorUtility(hex.length)];
+    }
+    console.log(hexColor);
+    setColor(hexColor);
+  };
+  const handleCreateRandomRgbColor = () => {
+    const r = randomColorUtility(256);
+    const g = randomColorUtility(256);
+    const b = randomColorUtility(256);
+
+    setColor(`rgb${r}, ${g}, ${b}`);
+  };
+
+  return (
+    <div className="w-screen h-screen pt-3" style={{ background: color }}>
+      <button
+        onClick={() => setTypeOfColor("hex")}
+        className="inline-flex items-center rounded-md bg-white px-3 py-3 ml-5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+      >
+        Create HEX Color
+      </button>
+      <button
+        onClick={() => setTypeOfColor("rgb")}
+        className="inline-flex items-center rounded-md bg-white px-3 py-3 ml-5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+      >
+        Create RGB Color
+      </button>
+      <button
+        onClick={
+          typeOfColor === "hex"
+            ? handleCreateRandomHexColor
+            : handleCreateRandomRgbColor
+        }
+        className="inline-flex items-center rounded-md bg-white px-3 py-3 ml-5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+      >
+        Create Random Color
+      </button>
+      <div className="flex justify-center">
+        <h3>{typeOfColor === "rgb" ? "RGB color" : "HEX color"}</h3>
+        <h1>{color}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default RandomColor;
