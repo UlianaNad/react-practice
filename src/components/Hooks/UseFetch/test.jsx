0796023cdffb4112b3288ae688eb3a7ae9d3a504
Uliana Nadorozhna <unadorozhna1@gmail.com>
2024-07-10 14:52:23 +0300

    useFetch hook+

--- src/components/Hooks/UseFetch/test.jsx
+++ src/components/Hooks/UseFetch/test.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import useFetch from "./index";
+import { Hourglass } from "react-loader-spinner";
 
 const UseFetchHookTest = () => {
   const { data, error, loading } = useFetch(
     "https://dummyjson.com/products",
     {}
   );
-  console.log(data, error, loading);
-  
-  return <div>Use fetch hook</div>;
+
+  return (
+    <div>
+      <h1>Use fetch hook</h1>
+      <ul>
+        {loading && <Hourglass />}
+        {error ? <h3>{error}</h3> : null}
+        {data && data.products && data.products.length > 0
+          ? data.products.map((item) => <li key={item.id}>{item.title}</li>)
+          : null}
+      </ul>
+    </div>
+  );
 };
 
 export default UseFetchHookTest;
